357fbc8c5a3027ec4b38e1ff378640b4a4e06499
paaraujogit <paaraujo@gmail.com>
2019-03-28 21:29:26 +0000

    Calculadora

---
+++ src/pa/calc/Calculadora.java
@@ -0,0 +1,18 @@
+package pa.calc;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		// OperEx Example
+		OperEx opEx = new OperEx();
+		double resEx = opEx.oper(1, 2);
+		System.out.println(resEx);
+		
+		// Oper02 - Solution
+		Oper02 op02 = new Oper02();
+		double res02 = op02.oper(10, 2);
+		System.out.println(res02);
+
+	}
+
+}
